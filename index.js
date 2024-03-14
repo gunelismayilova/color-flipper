1b83c1b9e478724f4d9ec4bd09d5735f92a47282
gnl77 <gunelismayilova726@gmail.com>
2024-03-14 06:13:47 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+
+    for (let i=1; i<=6; i++) {
+        let randomIndex = Math.floor(Math.random()*hexValues.length)  ;
+        hexColor += hexValues[randomIndex];
+        
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+
+})
+
+
+
+
